fd60a28fe3ac6421cdfa631a639acfa523e9b84c
kfang749@gmail.com <kfang749@gmail.com>
2025-12-19 22:59:57 -0500

    Completed section 15

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext db, IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u=>u.Id == userId, includeProperties: "Company"),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagementVM.ApplicationUser.Role == oldRole))
+            {
+                // Role was updated
+                ApplicationUser applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id, tracked: true);
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Including properties can exclude data that doesn't have that property populated, solution is to make that property optional.
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Company").ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            // Data is now showing regardless of whether company property is populated, but now it's causing issues because in the data table
+            // it tries to access company.Name when company is null.
+            foreach (var user in objUserList)
+            {
+                // Now roles are not filled because they are not in the application user model. To add that, we have to get the IDs from roles -> userRoles -> Users to match.
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+            return Json(new { data = objUserList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string userId)
+        {
+            var objFromDb = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == userId, tracked: true);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if(objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // user is locked and needs to be unlocked
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(10);
+            }
+
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+        #endregion
+
+    }
+}
