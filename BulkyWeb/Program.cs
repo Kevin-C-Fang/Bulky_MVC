c89e043c336d32d6c0037eaaab9942c9d249f9b0
kfang749@gmail.com <kfang749@gmail.com>
2025-12-25 10:18:41 -0500

    Completed program notes

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,32 +10,14 @@ using Bulky.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-/* Dependency Injection Lifetime
-    builder.Services.AddTransient<>();  // New service - every time it is requested
-    builder.Services.AddScoped<>();     // New service - once per request
-    builder.Services.AddSingleton<>();  // New service - once per application lifetime
- */
-
-// Add/configure entity framework core and adds to dependency injection so you don't have to handle creating/closing DbContext on each page.
-// Tools > NuGet Package manager > Package Manager Core
-// Enter "update-database" to create database
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Automatically inject the app settings section key/value into the properties in StripeSettings as long as the naming matches.
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 
-// Adds identity to service, adds all the database tables needed for identity, and that they will be managed using ApplicationDbContext 
-// builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
-// To add role to identity, you have to customize it by adding identity with the role.
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
-// Configurations that automatically redirect the options.xxxx to the specified routing path, must be added after adding identity.
-// This is due to those pages being within IdentityArea, since we don't want to copy the same files everywhere, just reference the path needed.
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = $"/Identity/Account/Login";
@@ -43,8 +25,6 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
-// Due to adding Nuget Authentication.Facebook, facebook authentication is made available.
-// Requires you to go to facebook.Developer and create an app to get configure app to allow sign in using facebook.
 builder.Services.AddAuthentication().AddFacebook(option =>
 {
     option.AppId = builder.Configuration.GetSection("FaceBook:AppId").Get<string>();
@@ -57,7 +37,6 @@ builder.Services.AddAuthentication().AddMicrosoftAccount(option =>
     option.ClientSecret = builder.Configuration.GetSection("Microsoft:ClientSecret").Get<string>();
 });
 
-// Configure application to add session.
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
@@ -66,13 +45,9 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-// Add DB Initializer to services so it can be found later on when app starts in SeedDatabase().
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 
-// Need to add and map razor pages for identity since it uses Razor pages and not MVC.
 builder.Services.AddRazorPages();
-
-// Register the IUnitOfWork to service so it can be dependency injected in CategoryController.
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
@@ -89,32 +64,22 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// Configure stripe within project by setting the API key.
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
 app.UseRouting();
-
-// Uses checking to see if user credentials are valid
 app.UseAuthentication();
-
-// Allows access to website based on user role.
 app.UseAuthorization();
-
-// Tells app to add session to request pipeline.
 app.UseSession();
 
 SeedDatabase();
 app.MapRazorPages();
 
-// Pattern is after domain with the default controller as home and action is index
-// id is null if not explicit
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
-// Method to call Initialize within DbInitializer to seed and push migrations when app starts by getting the service.
 void SeedDatabase()
 {
     using (var scope = app.Services.CreateScope())
